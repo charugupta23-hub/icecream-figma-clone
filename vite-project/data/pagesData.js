4cc02e374c748a7a4fdd9a994dfeea6da3535bb1
Charu Gupta <greatlikehell@gmail.com>
2025-06-20 01:28:06 +0530

    fix image paths

--- vite-project/data/pagesData.js
+++ vite-project/data/pagesData.js
@@ -1,45 +1,50 @@
-
 export const pagesData = [
   {
     flavor: "Strawberry cone",
     description: "Dive into our luscious strawberry scoop that refreshes your soul.",
     bgGradient: "linear-gradient(90deg, #be128d 0%, #ff6cee 100%)",
-    image: "public/assets/strawberry.png",
+    image: "/assets/strawberry.png",
     miniImages: [
-      "public/assets/strawberry.png",
-      "public/assets/blueberry.png",
-      "public/assets/chocolate.png",
-      "public/assets/evergreen.png"
+      "/assets/strawberry.png",
+      "/assets/blueberry.png",
+      "/assets/chocolate.png",
+      "/assets/evergreen.png"
     ]
   },
   {
     flavor: "Blueberry cone",
     description: "A fruity blueberry blast to make your tastebuds dance.",
     bgGradient: "linear-gradient(90deg, #236cda 0%, #47C4f6 100%)",
-    image: "public/assets/blueberry.png",
-    miniImages: [            "public/assets/strawberry.png",
-      "public/assets/blueberry.png",
-      "public/assets/chocolate.png",
-      "public/assets/evergreen.png"]
+    image: "/assets/blueberry.png",
+    miniImages: [
+      "/assets/strawberry.png",
+      "/assets/blueberry.png",
+      "/assets/chocolate.png",
+      "/assets/evergreen.png"
+    ]
   },
   {
     flavor: "Chocolate cone",
     description: "Classic chocolate that melts hearts with every bite.",
     bgGradient: "linear-gradient(90deg, #9c5c3f 0%, #e0bdb0 100%)",
-    image: "public/assets/chocolate.png",
-    miniImages: [      "public/assets/strawberry.png",
-      "public/assets/blueberry.png",
-      "public/assets/chocolate.png",
-      "public/assets/evergreen.png" ]
+    image: "/assets/chocolate.png",
+    miniImages: [
+      "/assets/strawberry.png",
+      "/assets/blueberry.png",
+      "/assets/chocolate.png",
+      "/assets/evergreen.png"
+    ]
   },
   {
     flavor: "Evergreen cone",
     description: "Fresh minty delight to chill your senses.",
     bgGradient: "linear-gradient(90deg, #2a9949 0%, #47f66e 100%)",
-    image: "public/assets/evergreen.png",
-    miniImages: [       "public/assets/strawberry.png",
-      "public/assets/blueberry.png",
-      "public/assets/chocolate.png",
-      "public/assets/evergreen.png"]
+    image: "/assets/evergreen.png",
+    miniImages: [
+      "/assets/strawberry.png",
+      "/assets/blueberry.png",
+      "/assets/chocolate.png",
+      "/assets/evergreen.png"
+    ]
   }
 ];
