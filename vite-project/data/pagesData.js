7f712b74605766b81e091b4357025caeb6c7dd6f
Charu Gupta <greatlikehell@gmail.com>
2025-06-20 01:20:21 +0530

    Your commit message here

--- vite-project/data/pagesData.js
+++ vite-project/data/pagesData.js
@@ -6,40 +6,40 @@ export const pagesData = [
     bgGradient: "linear-gradient(90deg, #be128d 0%, #ff6cee 100%)",
     image: "public/assets/strawberry.png",
     miniImages: [
-      "..public/assets/strawberry.png",
-      "..public/assets/blueberry.png",
-      "..public/assets/chocolate.png",
-      "..public/assets/evergreen.png"
+      "public/assets/strawberry.png",
+      "public/assets/blueberry.png",
+      "public/assets/chocolate.png",
+      "public/assets/evergreen.png"
     ]
   },
   {
     flavor: "Blueberry cone",
     description: "A fruity blueberry blast to make your tastebuds dance.",
     bgGradient: "linear-gradient(90deg, #236cda 0%, #47C4f6 100%)",
-    image: "/assets/blueberry.png",
-    miniImages: [            "..public/assets/strawberry.png",
-      "..public/assets/blueberry.png",
-      "..public/assets/chocolate.png",
-      "..public/assets/evergreen.png"]
+    image: "public/assets/blueberry.png",
+    miniImages: [            "public/assets/strawberry.png",
+      "public/assets/blueberry.png",
+      "public/assets/chocolate.png",
+      "public/assets/evergreen.png"]
   },
   {
     flavor: "Chocolate cone",
     description: "Classic chocolate that melts hearts with every bite.",
     bgGradient: "linear-gradient(90deg, #9c5c3f 0%, #e0bdb0 100%)",
-    image: "/assets/chocolate.png",
-    miniImages: [      "..public/assets/strawberry.png",
-      "..public/assets/blueberry.png",
-      "..public/assets/chocolate.png",
-      "..public/assets/evergreen.png" ]
+    image: "public/assets/chocolate.png",
+    miniImages: [      "public/assets/strawberry.png",
+      "public/assets/blueberry.png",
+      "public/assets/chocolate.png",
+      "public/assets/evergreen.png" ]
   },
   {
     flavor: "Evergreen cone",
     description: "Fresh minty delight to chill your senses.",
     bgGradient: "linear-gradient(90deg, #2a9949 0%, #47f66e 100%)",
-    image: "/assets/evergreen.png",
-    miniImages: [       "..public/assets/strawberry.png",
-      "..public/assets/blueberry.png",
-      "..public/assets/chocolate.png",
-      "..public/assets/evergreen.png"]
+    image: "public/assets/evergreen.png",
+    miniImages: [       "public/assets/strawberry.png",
+      "public/assets/blueberry.png",
+      "public/assets/chocolate.png",
+      "public/assets/evergreen.png"]
   }
 ];
